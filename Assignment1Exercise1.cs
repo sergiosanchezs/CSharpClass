4396339376fcf073f4160bd7cf6eb61f69a88f9a
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-01-15 11:54:05 -0500

    Create Assignment1Exercise1.cs

---
+++ Assignment1Exercise1.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Assignment1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // Excersice 1 - Write a program to print multiplication table of a number 5 (without loop)
+         const int OPERATOR1 = 5;
+         int result, operator2;
+         operator2 = 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+         operator2 += 1;
+         result = OPERATOR1 * operator2;
+         Console.WriteLine("{0} * {1} = {2}", OPERATOR1, operator2, result);
+      }
+   }
+}
