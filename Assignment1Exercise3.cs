a41db3da2d5104aafc515588b4838bf310b2ad88
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-01-15 12:26:26 -0500

    Create Assignment1Exercise3.cs

---
+++ Assignment1Exercise3.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Assignment1
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         // Excersice 3 - Write a program to calculate following calculations
+         
+         Console.WriteLine(" The operation (-1 + 4 * 6) = {0}", (-1 + 4 * 6));
+         Console.WriteLine(" The operation (( 35+ 5 ) % 7) = {0}", ((35 + 5) % 7));
+         Console.WriteLine(" The operation (14 + -4 * 6 / 11) = {0}", (14 + -4 * 6.0 / 11));
+         Console.WriteLine(" The operation (-1 + 4 * 6) = {0}", (2 + 15.0 / 6 * 1 - 7 % 2));
+
+      }
+   }
+}
