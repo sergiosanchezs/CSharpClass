63803bf8de908bec5bdb83423f966cfdc8042c16
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-02-18 10:30:24 -0500

    prime numbers exercise from user input startNumber and endNumber

--- Week5/Program.cs
+++ Week5/Program.cs
@@ -26,6 +26,35 @@ namespace Week5
          }
 
 
+         int num, ctr;
+         int startNumber, endNumber;
+         // Prime numbers
+         Console.WriteLine("Find the prime numbers within a range of numbers:");
+         Console.Write("Input starting number of range: ");
+         startNumber = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Input ending number of range : ");
+         endNumber = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine($"The prime numbers between {startNumber} and {endNumber} are :");
+
+
+
+         for (num = startNumber; num <= endNumber; num++)
+         {
+            ctr = 0;
+            for (i = 2; i <= (num/2); i++)
+            {
+               if (num % i == 0)
+               {
+                  ctr++;
+                  break;
+               }
+            }
+            if (ctr == 0 && num != 1)
+               Console.Write($"{num} ");
+         }
+         Console.WriteLine();
+
+
       }
    }
 }
