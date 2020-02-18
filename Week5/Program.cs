3e1224376848f2e33687dec39f537d43d85ed7a5
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-02-18 09:32:07 -0500

    week file exercises created

---
+++ Week5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Week5
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+
+         int i, j;
+
+         Console.WriteLine("Please, enter a number: ");
+         i = Convert.ToInt32( Console.ReadLine() );
+
+         for (j = 2; j < i; j++)
+         {
+            if (i % j == 0)
+            {
+               Console.WriteLine($"{i} no");
+               break;
+            }
+         }
+         if (j == i)
+         {
+            Console.WriteLine($"{i} yes");
+         }
+
+
+      }
+   }
+}
