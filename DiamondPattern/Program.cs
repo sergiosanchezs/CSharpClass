aad2b514a57b13c0adc3054bd9340e0e37a2ead2
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-03-04 18:52:34 -0500

    documenting and improving variable names of diamond method

--- DiamondPattern/Program.cs
+++ DiamondPattern/Program.cs
@@ -10,7 +10,7 @@ namespace DiamondPattern
          string symbol;
          userSize = getSize();
          symbol = getSymbol();
-         diamondV4(userSize, symbol);
+         diamondV5(userSize, symbol);
       }
 
       private static int getSize()
@@ -25,6 +25,42 @@ namespace DiamondPattern
          Console.WriteLine("Which symbol do you want to use in the diamond pattern:");
          symbol = (char)Console.Read() + " ";
          return symbol;
+      }
+      /// <summary>
+      /// This method prints a Diamond pattern specifying the 
+      /// size of the wide section and the the symbol of your
+      /// keyboard that you want to be printed.
+      /// </summary>
+      /// <param name="userSize">This is the size of the wide section of the diamond</param>
+      /// <param name="symbol">This is the symbol to be printed. For example, bydefault is '*'.</param>
+      private static void diamondV5(int userSize, string symbol = "* ")
+      {
+         int lineNo, spaces, symbolQty, leftSpaces;
+         bool isIncrementing;
+
+         isIncrementing = true;
+         leftSpaces = userSize - 1;
+
+         for (lineNo = isIncrementing ? 1 : leftSpaces;
+            (isIncrementing && lineNo <= userSize) || 
+            (!isIncrementing && lineNo >= 1);
+            lineNo = isIncrementing ? lineNo + 1 : lineNo - 1, 
+            leftSpaces = isIncrementing ? leftSpaces - 1 : leftSpaces + 1)
+         {
+            for (spaces = leftSpaces; spaces >= 1; spaces--)
+               Console.Write(" ");
+
+            for (symbolQty = isIncrementing ? 1 : lineNo;
+               (isIncrementing && symbolQty <= lineNo) || 
+               (!isIncrementing && symbolQty >= 1);
+               symbolQty = isIncrementing ? symbolQty + 1 : symbolQty - 1)
+               Console.Write(symbol);
+
+            if (lineNo == userSize)
+               isIncrementing = false;
+
+            Console.WriteLine();
+         }
       }
 
       private static void diamondV4(int userSize, string symbol)
