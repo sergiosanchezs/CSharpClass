2e8bff5550817fb2201f276d39e06b437662fc77
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-03-31 11:19:45 -0400

    class assignment done

---
+++ WFApp1/WFApp1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFApp1
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void calcBtn_Click(object sender, EventArgs e)
+      {
+
+         Square mySquare = new Square();
+         bool isNumber = false;
+         try
+         {
+            int length = Convert.ToInt32(lengthTBox.Text);
+            int width = Convert.ToInt32(widthTBox.Text);
+            int height = Convert.ToInt32(heightTBox.Text);
+            mySquare.setLength(length);
+            mySquare.setWidth(width);
+            mySquare.setHeight(height);
+            isNumber = true;
+         }
+         catch (Exception ex)
+         {
+            MessageBox.Show("Please fill out the length and width fields or put only numbers");
+         }
+
+         if (isNumber)
+            MessageBox.Show("The variables are \n\n"
+               + "Length: " + mySquare.getLength() + "\n"
+               + "Width: " + mySquare.getWidth() + "\n"
+               + "Height: " + mySquare.getHeight() + "\n\n"
+               + "The Area is " + mySquare.calculateSquareArea());
+      }
+   }
+}
---
+++ WFApp1/WFApp1/Rectangle.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFApp1
+{
+   public class Rectangle
+   {
+      public int newWidth;
+      public int newLength;
+      public int newHeight;
+
+      public Rectangle()
+      {
+         newWidth = 0;
+         newLength = 0;
+         newHeight = 0;
+      }
+
+      public Rectangle(int length, int width)
+      {
+         newWidth = width;
+         newLength = length;
+      }
+
+      public Rectangle(int length, int width, int height)
+      {
+         newWidth = width;
+         newLength = length;
+         newHeight = height;
+      }
+
+      public int getLength()
+      {
+         return newLength;
+      }
+      public void setLength(int length)
+      {
+         newLength = length;
+      }
+
+      public int getWidth()
+      {
+         return newWidth;
+      }
+
+      public void setWidth(int width)
+      {
+         newWidth = width;
+      }
+
+      public int getHeight()
+      {
+         return newHeight;
+      }
+      public void setHeight(int height)
+      {
+         newHeight = height;
+      }
+
+      public int calculateArea()
+      {
+         return (newLength * newWidth);
+      }
+   }
+}
